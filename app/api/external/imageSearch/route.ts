98d305251d18df5772cc87e458e7166af2d5f8a3
drago <dragodominguezfb@gmail.com>
2024-04-06 16:48:34 -0600

    Implement image search and registration page enhancements
    - Added image search API integration in `route.ts` using SerpApi for dynamically fetching images based on user queries. The implementation ensures that a search query is provided, processes the query to fetch image results, and formats the response to include URL, height, and width of each image, enhancing the application's external data interaction capabilities.
    - Enhanced the registration page in `page.jsx` by dynamically populating user information (email, first name, last name, and image) using URL search parameters, providing a personalized user registration experience. The implementation includes a structured approach to extract query parameters, utilize them to prefill the registration form, and display welcoming messages, thereby improving user onboarding flow.
    - Updated `package.json` and `package-lock.json` to include `serpapi` dependency, aligning with the new image search functionality's requirements and ensuring the application's dependencies are well-managed and up-to-date.
    
    These changes collectively improve the application's external API integration for image search and streamline the user registration process by providing a more interactive and personalized experience.
    
     Changes to be committed:
    	new file:   app/api/external/imageSearch/route.ts

---
+++ app/api/external/imageSearch/route.ts
@@ -0,0 +1,49 @@
+import { NextResponse } from "next/server";
+import { getJson } from "serpapi";
+
+const apiKeySearch = process.env.SERPAPI;
+
+export async function POST(request: Request) {
+  const { imageQuery } = await request.json();
+  try {
+    if (!imageQuery) {
+      return NextResponse.json({
+        success: false,
+        message: "Please provide a search query",
+      });
+    }
+
+    const response = await (async function () {
+      return await getJson(
+        {
+          api_key: apiKeySearch,
+          engine: "google_images",
+          google_domain: "google.com",
+          q: imageQuery,
+          hl: "en",
+          gl: "us",
+          safe: "active",
+        },
+        function (json) {
+          const images = json.images_results.map((item: any) => {
+            return {
+              url: item.thumbnail,
+              height: item.original_height,
+              width: item.original_width,
+            };
+          });
+          return images;
+        }
+      );
+    })();
+    return NextResponse.json({
+      success: true,
+      data: response,
+    });
+  } catch (error) {
+    return NextResponse.json({
+      message: "Error getting images",
+      error: error,
+    });
+  }
+}
